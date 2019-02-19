c7534bc59c6df61aacedc0554b13c81c04224631
Kuiying Wang <kuiying.wang@intel.com>
2019-02-19 15:00:11 +0800

    [WIP] Implement post code manager
    Implement method and properties defined in PostCode.interface.yaml
    under phosphor-dbus-interfaces/xyz/openbmc_project/State/Boot
    1. Method: std::vector<uint64_t> PostCode::getPostCodes(uint16_t index)
    2. Properties: CurrentBootCycleIndex/MaxBootCycleNum
    
    Change-Id: Ia89b9121983261fef5573092d890beb84626ceeb
    Signed-off-by: Kuiying Wang <kuiying.wang@intel.com>

---
+++ inc/post_code.hpp
@@ -0,0 +1,110 @@
+/*
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+#pragma once
+#include <fcntl.h>
+#include <unistd.h>
+#include <cereal/cereal.hpp>
+#include <experimental/filesystem>
+
+#include <xyz/openbmc_project/State/Boot/PostCode/server.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+
+#define MaxPostCodeCycles 100
+
+const static constexpr char *PostCodePath =
+    "/xyz/openbmc_project/State/Boot/Raw";
+const static constexpr char *PostCodeIntf =
+    "xyz.openbmc_project.State.Boot.Raw";
+const static constexpr char *PropertiesIntf =
+    "org.freedesktop.DBus.Properties";
+const static constexpr char *PostCodeListPath =
+    "/var/lib/phosphor-post-code-manager/";
+
+
+struct EventDeleter
+{
+    void operator()(sd_event *event) const
+    {
+        event = sd_event_unref(event);
+    }
+};
+using EventPtr = std::unique_ptr<sd_event, EventDeleter>;
+namespace fs = std::experimental::filesystem;
+
+using post_code =
+    sdbusplus::xyz::openbmc_project::State::Boot::server::PostCode;
+
+struct PostCode : sdbusplus::server::object_t<post_code>
+{
+    PostCode(sdbusplus::bus::bus& bus, const char* path,
+                 EventPtr &event) :
+        sdbusplus::server::object_t<post_code>(bus, path),
+        bus(bus), currentBootCycleIndex(0),
+        propertiesChangedSignal(
+            bus,
+            sdbusplus::bus::match::rules::type::signal() +
+                sdbusplus::bus::match::rules::member("PropertiesChanged") +
+                sdbusplus::bus::match::rules::path(
+                    PostCodePath) +
+                sdbusplus::bus::match::rules::interface(PropertiesIntf),
+            [this](sdbusplus::message::message &msg) {
+                phosphor::logging::log<phosphor::logging::level::INFO>(
+                    "PostCode propertiesChangedSignal callback function is "
+                    "called...");
+                std::string objectName;
+                std::map<std::string, sdbusplus::message::variant<uint64_t>> msgData;
+                msg.read(objectName, msgData);
+                // Check if it was the Value property that changed.
+                auto valPropMap = msgData.find("Value");
+                {
+                    if (valPropMap != msgData.end())
+                    {
+                        phosphor::logging::log<phosphor::logging::level::INFO>(
+                            "find value");
+                        this->savePostCodes(sdbusplus::message::variant_ns::get<uint64_t>(valPropMap->second));
+                    }
+                }
+        })
+    {
+        phosphor::logging::log<phosphor::logging::level::INFO>(
+                    "PostCode is created");
+        deserialize(fs::path(PostCodeListPath + "CurrentBootCycleIndex"));
+        maxBootCycleNum(MaxPostCodeCycles);
+        if (currentBootCycleIndex() >= maxBootCycleNum())
+        {
+            currentBootCycleIndex(1);
+        } else{
+            currentBootCycleIndex(currentBootCycleIndex() + 1);
+        }
+    }
+    ~PostCode()
+    {
+
+    }
+
+    std::vector<uint64_t> getPostCodes(uint16_t index) override; 
+
+  private:
+    sdbusplus::bus::bus& bus;
+    std::vector<uint64_t> postCodes;
+    void savePostCodes(uint64_t code);
+    sdbusplus::bus::match_t propertiesChangedSignal;
+    fs::path serialize(const fs::path& path);
+    bool deserialize(const fs::path& path);
+    bool deserializePostCodes(const fs::path& path, std::vector<uint64_t> &codes);
+};
+
---
+++ src/post_code.cpp
@@ -0,0 +1,102 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+#include "post_code.hpp"
+std::vector<uint64_t> PostCode::getPostCodes(uint16_t index)
+{
+    std::vector<uint64_t> codes;
+
+    phosphor::logging::log<phosphor::logging::level::INFO>(
+                    "getPostCodes is called");
+    if (currentBootCycleIndex() == index)
+        return postCodes;
+    deserializePostCodes(fs::path(PostCodeListPath + std::to_string(index)), codes);
+    return codes;
+}
+void PostCode::savePostCodes(uint64_t code)
+{
+    phosphor::logging::log<phosphor::logging::level::INFO>(
+                    "saveToPostCodeList is called");
+    postCodes.push_back(code);
+    serialize(fs::path(PostCodeListPath));
+    return;
+}
+
+fs::path PostCode::serialize(const fs::path& path)
+{
+    std::ofstream os(path.c_str() + "CurrentBootCycleIndex", std::ios::binary);
+    cereal::JSONOutputArchive oarchive(os);
+    oarchive(currentBootCycleIndex());
+
+    std::ofstream osPostCodes(path.c_str() + std::to_string(currentBootCycleIndex()), std::ios::binary);
+    cereal::JSONOutputArchive oarchivePostCodes(osPostCodes);
+    oarchivePostCodes(postCodes());
+    return path;
+}
+
+bool PostCode::deserialize(const fs::path& path)
+{
+    try
+    {
+        if (fs::exists(path))
+        {
+            std::ifstream is(path.c_str(), std::ios::in | std::ios::binary);
+            cereal::JSONInputArchive iarchive(is);
+            iarchive(currentBootCycleIndex);
+            return true;
+        }
+        return false;
+    }
+    catch (cereal::Exception& e)
+    {
+        log<level::ERR>(e.what());
+        fs::remove(path);
+        return false;
+    }
+    catch (const fs::filesystem_error& e)
+    {
+        return false;
+    }
+
+    return false;
+}
+
+bool PostCode::deserializePostCodes(const fs::path& path, std::vector<uint64_t> &codes)
+{
+    try
+    {
+        if (fs::exists(path))
+        {
+            std::ifstream is(path.c_str(), std::ios::in | std::ios::binary);
+            cereal::JSONInputArchive iarchive(is);
+            iarchive(codes);
+            return true;
+        }
+        return false;
+    }
+    catch (cereal::Exception& e)
+    {
+        log<level::ERR>(e.what());
+        fs::remove(path);
+        return false;
+    }
+    catch (const fs::filesystem_error& e)
+    {
+        return false;
+    }
+
+    return false;
+}
+
